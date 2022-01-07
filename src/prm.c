2344d187f79fb81e5b1ff8e1cb85f05662ddcf1d
Qowevisa <qowevisa@gmail.com>
2022-01-07 15:11:44 +0200

    Fix some problems in prm

--- src/prm.c
+++ src/prm.c
@@ -3,16 +3,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-int main(int argc, char *argv[]) {
+int main() {
 	if (!isatty(fileno(stdin))) {
-		char *buff = NULL;
+		char *buff = (char*)calloc(2048, 1);
 		size_t n = 0;
-		size_t buff_size = getline(&buff, &n, stdin);
-		while (buff_size != -1) {
+		while (getline(&buff, &n, stdin) != -1) {
 			// getting the name without '\n' char
-			char *nof = (char*)malloc(buff_size - 1);
-			for (int i = 0; i < buff_size - 1; i++)
-				nof[i] = buff[i];
+			char *nof = (char*)calloc(2048, 1);
+            size_t i = 0;
+            while (buff[i] != '\n') {
+                nof[i] = buff[i];
+                i++;
+            }
 
 			pid_t id = fork();
 			// Child process ID
@@ -21,12 +23,11 @@ int main(int argc, char *argv[]) {
 				int val = execvp(args[0], args);
 				if (val == -1) {
 					printf("Error occured in execvp!\n");
-					exit(2);
+					return 2;
 				}
 				// if for some reasons it won't end by calling execvp.
 				return 0;
 			}
-			buff_size = getline(&buff, &n, stdin);
 		}
 	}
 	return 0;
